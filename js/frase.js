2d766c608d2b8c030ab0b7d26a04353aef70cd52
Nadinne Cavalcante <nadinnecavalcantesilva@gmail.com>
2023-04-30 00:03:19 -0300

    alura_typer
    Projeto desenvolvido durante curso sobre jQuery da Alura.
    
    Um jogo onde o usuário deve reescrever a frase apresentada durante  o tempo determinado. Sendo possível escolher dentre algumas opções de frase, cada uma com seu tempo especifico, registrar a pontuação em um placar, definindo o nome do participante.
    
    Desenvolvido com:
    
    HTML5
    CSS3
    JQuery v3.1.0: https://jquery.com/
    Materialize v0.97.6 : http://materializecss.com

---
+++ js/frase.js
@@ -0,0 +1,50 @@
+$("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscarFrase);
+
+function fraseAleatoria(){
+    $("#spinner").toggle();
+    
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria).fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+        }, 3000);   
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+        
+}
+
+function trocaFraseAleatoria(data){
+        var frase = $(".frase");
+        var numAleatorio = Math.floor(Math.random() * data.length);
+        frase.text(data[numAleatorio].texto);
+        atualizaTamanhoFrase();
+        atualizaTempoInicial(data[numAleatorio].tempo);
+}
+
+function buscarFrase(){
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+    /* console.log("ID da frase: " + fraseId); */
+    var dados = { id: fraseId};
+
+    $.get("http://localhost:3000/frases",dados,trocaFrase)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+        }, 3000);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data){
+    var frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
+}
